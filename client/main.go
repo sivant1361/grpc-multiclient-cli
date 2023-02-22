c47cd2af6f1394bc9e41865dc0b3774562aa4b2a
sivant1361 <55569255+sivant1361@users.noreply.github.com>
2023-02-22 16:06:15 +0530

    grpc multiple client

---
+++ client/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	message "go-cli-chat/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const port = ":5050"
+
+func getInput() string {
+	reader := bufio.NewReader(os.Stdin)
+	sentence, _ := reader.ReadString('\n')
+	sentence = strings.TrimSpace(sentence)
+	return sentence
+}
+
+func ChatClientFunc(client message.MessageServiceClient) {
+	fmt.Println("\nClient Cli")
+	fmt.Println("**********")
+	fmt.Println()
+
+	fmt.Print("Enter username: ")
+
+	username := getInput()
+	connect := username
+
+	stream, err := client.SendReply(context.Background())
+	if err != nil {
+		log.Fatalf("could not send reply: %v", err)
+	}
+
+	go func() {
+		for {
+			message, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Error while streaming %v", err)
+			}
+
+			from := message.GetFrom()
+			to := message.GetTo()
+			content := message.GetContent()
+
+			if from == to {
+				fmt.Print("\r", "server> ", content, "\n", username+"> ")
+				connect = username
+			} else {
+				fmt.Print("\r", from+"> ", content, "\n", username+"> ")
+			}
+		}
+	}()
+
+	fmt.Print(username + "> ")
+	for {
+		inp := getInput()
+		if strings.Contains(inp, "connect") {
+			if strings.Contains(inp, "disconnect") {
+				connect = username
+			} else {
+				connect = strings.Split(inp, " ")[1]
+				fmt.Print(username + "> ")
+				inp = getInput()
+			}
+		}
+		if inp == "exit" {
+			fmt.Println("")
+			break
+		} else {
+			req := &message.Message{
+				Content: inp,
+				From:    username,
+				To:      connect,
+			}
+			if err := stream.Send(req); err != nil {
+				log.Fatalf("Error while sending %v", err)
+			}
+			fmt.Print(username + "> ")
+		}
+		time.Sleep(time.Second / 2)
+	}
+	stream.CloseSend()
+	log.Printf("Chating with server is finished")
+}
+
+func main() {
+	conn, err := grpc.Dial(port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	c := message.NewMessageServiceClient(conn)
+
+	log.Println("Initial connect done")
+	ChatClientFunc(c)
+
+}
---
+++ server/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	message "go-cli-chat/proto"
+	"io"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type messageServer struct {
+	message.MessageServiceServer
+}
+
+var channelMapper = make(map[string]chan *message.Message)
+var ActiveMapper = make(map[string]bool)
+
+// var messageQueue []string
+
+func SenderChat(stream message.MessageService_SendReplyServer, ch chan *message.Message) error {
+	for {
+		value, ok := <-ch
+		if ok {
+
+			from := value.GetFrom()
+			to := value.GetTo()
+			content := value.GetContent()
+
+			res := &message.Message{
+				Content: content,
+				From:    from,
+				To:      to,
+			}
+
+			if err := stream.Send(res); err != nil {
+				return err
+			}
+		}
+	}
+}
+
+func (s *messageServer) SendReply(stream message.MessageService_SendReplyServer) error {
+
+	flag := false
+
+	for {
+		req, err := stream.Recv()
+		if flag == false {
+			log.Printf("Got connected with : %v", req.GetFrom())
+			channelMapper[req.GetFrom()] = make(chan *message.Message)
+			go SenderChat(stream, channelMapper[req.GetFrom()])
+			ActiveMapper[req.GetFrom()] = true
+			flag = true
+		}
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		log.Printf("Request from : %v", req.GetFrom())
+		if ActiveMapper[req.GetTo()] != true {
+			req.Content = "Sender named " + req.To + " not there"
+			req.To = req.GetFrom()
+		}
+		channelMapper[req.GetTo()] <- req
+		// messageQueue = append(messageQueue, req.Content)
+	}
+}
+
+const port = ":5050"
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+
+	if err != nil {
+		log.Fatalf("Failed to listen %v", err)
+	}
+
+	gs := grpc.NewServer()
+
+	message.RegisterMessageServiceServer(gs, &messageServer{})
+	log.Printf("Server started at %v", lis.Addr())
+
+	if err := gs.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve %v", err)
+	}
+}
